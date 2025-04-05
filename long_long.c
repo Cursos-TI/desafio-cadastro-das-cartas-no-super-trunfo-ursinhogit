4319f403c0679cda2caf51f76bf8dc3053dfc97d
ursinhogit <vvaniaoliveira@gmail.com>
2025-04-05 02:13:34 +0000

    dasafio mestre

---
+++ long_long.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    double numeroPreciso = 3.144159265358979;
+    long double numeroMuitoPreciso =3.14159226535879;
+
+    printf("NumeroPreciso(double): %15f \n", numeroPreciso);
+    printf("NumeroMuitoPreciso (long double): %21lf \n", numeroMuitoPreciso);
+
+    return 0;
+
+
+}
--- super_trunfo.c
+++ super_trunfo.c
@@ -10,14 +10,14 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.]
-    char estado,código, cidade; 
+    char estado,códigodaCidade; 
     int população,PontosTuristicos;
-    float área,pib,Cartas,densidadepopulacional,pibpercapito;
+    float área,pib,Cartas,densidadepopulacional,pibPerCapito;
      
     // Cadastro  char carta[4],carta[4] estado[80], código [20] ,Cidade[80];
     char carta1[4],carta2[4],estado1[80],estado2[20],código1[20],código2 [20],Cidade1[80],Cidade2[80];
     int população1, população2,Numero1,numero2,PontosTuristicos1,pontosTuristicos2;
-    float área1,área2,pib1,pib2;densidadepopulacional,densidadepopulacional2, pibpercapito1,pibpercapito2;
+    float área1,área2,pib1,pib2;densidadepopulacional1,densida, pibPerCapito1,pibPerCapito2;
     
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
@@ -64,21 +64,73 @@ int main() {
       float  população2=6748000;
       float quociente = (float)área2 /população2;
 
-      printf("Quociente:%f.2f\n" quociente);
+      printf("Quociente:%f.2f\n"quociente);
 
       float pib1 = 699.28 ;
       float população1 =1232500;
       float quociente = (float)pib1/população1;
 
-      printf("Quociente:%f.2f\n" quociente);
+      printf("Quociente:%f.2f\n"quociente);
 
       float pib2 = 300.50 ;
       float população2 =1232500;
       float quociente = (float)pib2/população2;
 
-      printf("Quociente:%f.2f\n" quociente);
+      printf("Quociente:%f.2f\n"quociente);
 
+      float população1=12325000;
+      float população2=6748000;
+ 
+      float área1 =1521.11;
+      float área2 =1200.25;
+      
+      float pib1=699.28;
+      float pib2=300.50;
 
+      float pibPerCapito1=56724.32;
+      float pibPerCapito2=44532.91;
+
+      float densidadePopulacional1=5622.24;
+      float densidadePopulacional2=8102.47;
+
+      float pontosTuristicos1=30;
+      float pontosTuristicos2=50;
+
+      printf("população1>população2:%d\n",Numero1>numero2);
+      printf ("numero1==numero2:%d\n",Numero1==numero2);
+
+      printf("área1>área2:%d\n",área1>área2);
+      printf ("área1==área2:%d\n",área1==área2);
+      
+      printf("pib1>pib2:%d\n",pib1>pib2);
+      printf ("pib1==pib2:%d\n",pib1==pib2);
+
+      printf("pibPerCapito1>pibPerCapito2:%d\n",pibPerCapito1>pibPerCapito2);
+      printf ("pibPerCapito1==pibPerCapito2:%d\n",pibPerCapito1==pib2);
+
+      printf("densidadePopulacional1>desnidadePopulacional2:%d\n",densidadePopulacional1>densidadePopulacional2);
+      printf ("densidadePopulacional1==densidadePopulacional2:%d\n",densidadePopulacional1==densidadePopulacional2);
+      
+      printf("pontosTuristicos1>pontosTuristicos2:%d\n",pontosTuristicos1>pontosTuristicos2);
+      printf ("pontosTuristicos1==pontosTuristicos2:%d\n",pontosTuristicos1==pontosTuristicos2);
+
+      printf("população1>população2");
+      printf("resultado=população1>população2");
+
+      printf (área1>área2);
+      printf ("resultado=área1>área2");
+
+      printf("pib1>pib2");
+      printf("resultado=pib1>pib2");
+
+      printf("pibPerCapito1>pibPerCapito2");
+      printf("resultado=pibPerCapito1>pibPrCapito2");
+
+      printf("densidadePopilacional1>densidadepopulacional2");
+      printf("resultado=densidadePopulacional1>densidadePopulacional2");
+
+      printf("pontosturisticos1>pontosTuristicos2");
+      printf("resultado=pontosTuristicos1>pontosTuristicos2");
 
 
 
@@ -87,5 +139,12 @@ int main() {
 
 
       
-   return 0;
+      
+
+
+
+
+
+return 0;
+
 }
