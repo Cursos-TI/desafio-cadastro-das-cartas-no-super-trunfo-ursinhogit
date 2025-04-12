6380171a0e0284850a63cf456f9b57a0c1be1adb
ursinhogit <vvaniaoliveira@gmail.com>
2025-04-12 15:22:30 +0000

    date h '#' will be ignored, and an empty message aborts the commit.

---
+++ desafio_jokenpo.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    char carta1, carta2;
+    int populacao1,populacao2;
+    int Jogador1,jogador2;
+
+    printf("Bem vindo ao jogo!");
+    printf(voce deve escolher um jogador\n");
+    printf("jogador1 \n");
+    printf("jogador2 \n");
+    
+    printf("Se a população1>população2,digite:jogador1 ganhou!\n");
+    senão;
+    printf("digite:Infelizmente você perdeu!\n");
+    tambem;
+    printf("empatou!\n);
+
+    switch (escolha a carta)
+    {
+    case'C':
+    case 'c':
+    printf("Você escolheu carta1\n ");
+    resultado1= carta1>carta2?1:0;
+    break;
+    case 'P':
+    case 'p':
+    resultado2=populacao1<populacao2?1:0;
+    break;
+    case 'J':
+    case 'j':
+    resultado3:Jogador1>jogador2?1:0;
+    break;
+    default:
+    printf("Opção inválida!\n");
+    break;
+    }
+    if(resultado1 && resultado2);
+    printf("Parabéns você venceu!\n");
+    
+    }else if (resultado1!==resultado2){ 
+    printf("Empatou!\n");
+   }else{
+    printf("infelizmente você perdeu!\n");
+ }
+
+ return.0;
+ }
+
+
+     
+
+
